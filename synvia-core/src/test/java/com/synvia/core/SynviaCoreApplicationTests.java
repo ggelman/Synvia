be86d0952fc082230099219352b8c650b1479c19
Giulia Helena Gelman F Baracat <162526097+ggelman@users.noreply.github.com>
2025-10-27 18:56:17 -0300

    Add files via upload

---
+++ synvia-core/src/test/java/com/synvia/core/SynviaCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.synvia.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynviaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test checks if the Spring application context loads successfully.
+		// If there are any issues with the configuration or beans, this test will fail.
+		// No additional code is needed here as we are just verifying the context load.
+	}
+
+}
