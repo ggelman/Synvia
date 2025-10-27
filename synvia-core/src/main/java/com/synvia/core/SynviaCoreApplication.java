ecaece2431f86946c3f23b4b3c5c0d8ec2093a0c
Giulia Helena Gelman F Baracat <162526097+ggelman@users.noreply.github.com>
2025-10-27 19:02:06 -0300

    Add files via upload

---
+++ synvia-core/src/main/java/com/synvia/core/SynviaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.synvia.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynviaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynviaCoreApplication.class, args);
+	}
+
+}
